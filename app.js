6ec71042e143408ba5697b688962cf1206232c09
Ivan <ivanchernetskey@mail.ru>
2021-02-17 12:19:03 +0300

    we added firebase and see in console what is storage(part of firebase)  wfefwe

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
+import firebase from 'firebase/app'
+import 'firebase/storage'
 import {upload} from './upload.js'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDePgMUW4OEovNP8VEhy8zMzjlmicfvnIQ",
+    authDomain: "fe-upload-74b56.firebaseapp.com",
+    projectId: "fe-upload-74b56",
+    storageBucket: "fe-upload-74b56.appspot.com",
+    messagingSenderId: "24448036258",
+    appId: "1:24448036258:web:3b6e526ae6ee3faf3896fb"
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+
+const storage = firebase.storage()
+
+console.log(storage)
 
 upload('#file', {
     multi: true,
